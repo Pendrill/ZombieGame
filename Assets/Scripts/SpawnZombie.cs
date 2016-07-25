a750e23c935a8aff26b6fae1d9def000dcecb727
Aleksei Waddington <amw694@nyu.edu>
2016-07-25 16:32:46 -0400

    Added a spawner. Zombies will spawn from it, and follow the player. Public variables are present for delay and interval.

---
+++ Assets/Scripts/SpawnZombie.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SpawnZombie : MonoBehaviour {
+
+	public GameObject ZombieClone;
+	public Transform spawner;
+	public float delayTime=5f;
+	public float spawnInterval=2f;
+	// Use this for initialization
+	void Start () {
+		InvokeRepeating ("cloneZombie", delayTime, spawnInterval);
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+	void cloneZombie(){
+		Instantiate(ZombieClone, spawner.position , Quaternion.Euler(0f,0f,0f));
+	}
+}
