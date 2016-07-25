b0a48f18f7054825649186ab654b77f4b51f97c0
Aleksei Waddington <amw694@nyu.edu>
2016-07-25 16:10:49 -0400

    Added more scripts. Player can move around, and will face the mouse pointer. One Zombie is in the scene. They will follow the player character until they overlap.

---
+++ Assets/Scripts/PlayerRotate.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerRotate : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		Vector3 cursorPositionInWorld = Camera.main.ScreenToWorldPoint (Input.mousePosition);
+		Vector3 lookDirection = cursorPositionInWorld - transform.position;
+		lookDirection.z = 0f;
+		transform.up = lookDirection;
+	}
+}
---
+++ Assets/Scripts/ZombieFollow.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ZombieFollow : MonoBehaviour {
+
+	public Transform player;
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Vector3.Distance (transform.position, player.position) > 0.1f) {
+			transform.position += Vector3.Normalize (player.position - transform.position) * Time.deltaTime;
+		}
+	}
+	void OnDrawGizmos(){
+		Gizmos.DrawLine (transform.position, player.position);
+	}
+}
