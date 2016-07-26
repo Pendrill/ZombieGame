62c59d2e3467a0f2e69ed0ca1da86a707d9bf905
Aleksei Waddington <Aleksei Waddington>
2016-07-26 19:43:52 -0400

    5/6 parts complete. Now for the shooting.

---
+++ Assets/Scripts/RestartGame.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+public class RestartGame : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetKey (KeyCode.R)) {
+			int currentSceneNumber = SceneManager.GetActiveScene ().buildIndex;
+			SceneManager.LoadScene (currentSceneNumber);
+
+		}
+	}
+}
